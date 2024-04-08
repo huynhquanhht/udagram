be6281cde473c2a0e2b5e6c5fb2bc47b7d7839fc
huynhquanhht <huynhquanhht@gmail.com>
2024-04-08 19:59:59 +0700

    Added k8s

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3a8d30cc65ff45e1a79425355785871-1709499825.us-east-1.elb.amazonaws.com'
 };
 
 /*
